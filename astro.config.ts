5201cd8942da44a5f2dd7e5720760741e265c85c
zhangkunpeng <2300549455@qq.com>
2025-09-13 13:17:21 +0800

    优化

--- astro.config.ts
+++ astro.config.ts
@@ -23,9 +23,6 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.url,
-	redirects: {
-		"/": "/posts/",
-	},
 	image: {
 		domains: ["webmention.io"],
 	},
